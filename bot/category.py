fe5d290bcb3e5a706a74bcac4e3bda91057c976c
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-12-31 23:18:34 +0700

    Create another command, update to handle the one-arg command

--- bot/category.py
+++ bot/category.py
@@ -57,6 +57,12 @@ class ModCategory(Category):
         # Check for a specific role in the member
         return any([i.id == config.mod_role_id for i in message.author.roles])
 
+class RequestCategory(Category):
 
+    name = "request"
+    prefix = None
+
+    def check_permissions(self, message: discord.Message) -> bool:
+        return True
 if __name__ == "__main__":
     print("I had a dream where I was fighting Chuck Norris. That day I woke up with scars.")
