4c981c355ad54e96e7e33f6db4c468423bcb504e
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-01-01 12:55:06 +0100

    Updated to use a regular expression
    Time is now specified with: „1d2h30m2s“.
    This means 1 day, 2 hours, etc.

--- bot/commands/utility/remind.py
+++ bot/commands/utility/remind.py
@@ -2,24 +2,27 @@
 from bot.config import Config, Embed
 from bot.base import Command
 from asyncio import sleep
+from re import match
 
 class cmd(Command):
     """ A discord command instance. """
 
     name = "remind"
-    usage = "remind <reminder> [hours] [*minutes]"
+    usage = "remind <*time> <reminder>"
     description = "Reminds the user of something."
 
     async def execute(self, arguments, message) -> None:
+        timeStr = arguments[0]
+        regEx = r"(?:(?P<days>\d+)d)?(?:(?P<hours>\d+)h)?(?:(?P<minutes>\d+)m)?(?:(?P<seconds>\d+)s)?"
+        times = match(regEx, timeStr).groupdict()
+        d = int(times["days"]) if times["days"] else 0
+        h = int(times["hours"]) if times["hours"] else 0
+        m = int(times["minutes"]) if times["minutes"] else 0
+        s = int(times["seconds"]) if times["seconds"] else 0
+        time = d * 24 * 3600 + h * 3600 + m * 60 + s
+        
         embed = Embed(title="New Reminder", description="Reminder set!")
-        await message.reply(embed=embed)
-        size = len(arguments)
-        if size == 2:
-            time = 60*30
-        elif size == 3 and arguments[2] == "":
-            time = 60*60*float(arguments[1])
-        else:
-            time = 60*60*float(arguments[1])+60*float(arguments[2])
+        await message.reply(embed=embed)        
         await sleep(time)
-        embed = Embed(title=arguments[0], description="This is your reminder.")
+        embed = Embed(title=arguments[1], description="This is your reminder.")
         await message.channel.send(f"<@{message.author.id}>", embed=embed)
