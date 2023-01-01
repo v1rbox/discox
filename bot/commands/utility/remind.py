dd821ff8c19c491c2bd5f6681b9393568cdf72c8
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-01-01 17:12:14 +0100

    Added timestamp and message link to bot messages

--- bot/commands/utility/remind.py
+++ bot/commands/utility/remind.py
@@ -3,6 +3,7 @@ from bot.config import Config, Embed
 from bot.base import Command
 from asyncio import sleep
 from re import match
+import time
 
 class cmd(Command):
     """ A discord command instance. """
@@ -20,10 +21,12 @@ class cmd(Command):
         h = int(times["hours"]) if times["hours"] else 0
         m = int(times["minutes"]) if times["minutes"] else 0
         s = int(times["seconds"]) if times["seconds"] else 0
-        time = d * 24 * 3600 + h * 3600 + m * 60 + s
+        reminderTime = d * 24 * 3600 + h * 3600 + m * 60 + s
+        timestamp = int(time.time()) + reminderTime
         
-        embed = Embed(title="New Reminder", description="Reminder set!")
-        await message.reply(embed=embed)        
-        await sleep(time)
-        embed = Embed(title=arguments[1], description="This is your reminder.")
+        embed = Embed(title="New Reminder", description=f"Reminder set to <t:{timestamp}:f>, wich is in <t:{timestamp}:R>.")
+        url = await message.reply(embed=embed).jump_url()
+        await sleep(reminderTime)
+        embed = Embed(title=arguments[1], description=f"""This is your reminder.
+        If you wan't to know the context, here is the link: {url}""")
         await message.channel.send(f"<@{message.author.id}>", embed=embed)
