fcb1930c5300ead5d971aec0b1f58199777a077f
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-01-03 00:42:31 +0100

    Added change_presence category

--- bot/category.py
+++ bot/category.py
@@ -56,6 +56,15 @@ class DistroCategory(Category):
 
     def check_permissions(self, message: discord.Message) -> bool:
         return True
+    
+class PresenceCatwgory(Category):
+    """ A command category instance. """
+    name = "change_presence"
+    prefix = "change"
+
+    def check_permissions(self, message: discord.Message) -> bool:
+        # Check for a specific role in the member
+        return any([i.id == config.mod_role_id for i in message.author.roles])
 
 
 class ModCategory(Category):
