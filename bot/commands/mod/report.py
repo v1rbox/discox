b5449df4646151839152fd14b317b1003805acb5
Code beautifier <logicguy1@users.noreply.github.com>
2023-01-16 15:19:47 +0000

    Beautified

--- bot/commands/mod/report.py
+++ bot/commands/mod/report.py
@@ -1,9 +1,10 @@
+import asyncio
+import datetime
+import time
+
 from bot.base import Command
 from bot.config import Config, Embed
 
-import time
-import datetime
-import asyncio
 
 class cmd(Command):
 
@@ -14,7 +15,9 @@ class cmd(Command):
     async def execute(self, arguments, message) -> None:
         await message.delete()
         member = await message.guild.fetch_member(arguments[0])
-        await member.timeout(datetime.timedelta(hours=3), reason=f"Report - {message.author}")
+        await member.timeout(
+            datetime.timedelta(hours=3), reason=f"Report - {message.author}"
+        )
 
         ts = int(time.time())
 
@@ -25,13 +28,15 @@ class cmd(Command):
         embedr.set_color("red")
 
         report = await message.author.send(embed=embedr)
-        msg = await message.author.send("Please enter the reason for the report, you can also provide context using message links.")
-        
+        msg = await message.author.send(
+            "Please enter the reason for the report, you can also provide context using message links."
+        )
+
         def check(m):
             return m.author.id == message.author.id and m.channel == msg.channel
 
         try:
-            msg = await self.bot.wait_for('message', timeout=600.0, check=check)
+            msg = await self.bot.wait_for("message", timeout=600.0, check=check)
         except asyncio.TimeoutError:
             await msg.channel.send("Timed out.")
             return
@@ -50,7 +55,9 @@ class cmd(Command):
             return user == message.author and str(reaction.emoji) in ["❌", "✅"]
 
         try:
-            reaction, user = await self.bot.wait_for('reaction_add', timeout=600.0, check=check)
+            reaction, user = await self.bot.wait_for(
+                "reaction_add", timeout=600.0, check=check
+            )
         except asyncio.TimeoutError:
             await channel.send("Timed out.")
             return
@@ -58,13 +65,15 @@ class cmd(Command):
         if str(reaction.emoji) == "✅":
             channel = await self.bot.fetch_channel(Config.report_channel_id)
             await channel.create_thread(
-                name=f"{arguments[0]} - Report", 
-                embed=embedr, 
-                applied_tags=[channel.available_tags[0]]
+                name=f"{arguments[0]} - Report",
+                embed=embedr,
+                applied_tags=[channel.available_tags[0]],
             )
             await msg.send("Report has been posted")
         else:
             await msg.send("Report Canseled")
             # Remove timeout
-            await member.timeout(datetime.timedelta(seconds=3), reason=f"Report Canseled - {message.author}")
-
+            await member.timeout(
+                datetime.timedelta(seconds=3),
+                reason=f"Report Canseled - {message.author}",
+            )
