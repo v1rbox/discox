e6b472416145fa51071479d57162119d05c21cfe
Demir Değerli <demirdegerli@protonmail.com>
2023-01-26 14:59:04 +0300

    beautify

--- bot/__main__.py
+++ bot/__main__.py
@@ -249,15 +249,15 @@ def main() -> None:
             user, timestamp, remindMsg, channel, message = reminder
             channelObj = await bot.fetch_channel(channel)
             url = f"https://discord.com/channels/{channelObj.guild.id}/{channel}/{message}"
-	    embed = Embed(title=parse.unquote(remindMsg), description=f"""This is your reminder.
-            If you want to know the context, [here]({url}) is the link.""")
+		embed = Embed(title=parse.unquote(remindMsg), description=f"""This is your reminder.
+If you want to know the context, [here]({url}) is the link.""")
 	    if (timestamp > int(time())):
-            	embed = Embed(title=parse.unquote(remindMsg), description=f"""This was your reminder from (<t:{timestamp}:R>) Sorry for being late.
-            If you want to know the context, [here]({url}) is the link.""")
+		embed = Embed(title=parse.unquote(remindMsg), description=f"""This was your reminder from (<t:{timestamp}:R>) Sorry for being late.
+If you want to know the context, [here]({url}) is the link.""")
             await channelObj.send(f"<@{user}>", embed=embed)
             await cursor.execute("DELETE FROM reminders WHERE User = ? AND Timestamp = ?", (user, timestamp))
         await db.commit()
-        await cursor.close()	
+        await cursor.close()
 
     @bot.event
     async def on_message(message: discord.Message):
