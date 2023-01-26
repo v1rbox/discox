f5a644aafb1483f9794e04a257984011feb052f7
Demir Değerli <demirdegerli@protonmail.com>
2023-01-26 17:21:13 +0300

    use `self.bot` and `self.db`

--- bot/tasks/reminders.py
+++ bot/tasks/reminders.py
@@ -5,8 +5,6 @@ from time import time
 
 from bot.base import Task
 from bot.config import Config, Embed
-from discord import client as bot
-from aiosqlite import Connection as db
 
 
 class TaskLoop(Task):
@@ -14,17 +12,13 @@ class TaskLoop(Task):
 
     @tasks.loop(seconds=30)
     async def execute(self):
-        cursor = await db.cursor()
-       	await cursor.execute("SELECT * FROM reminders ORDER BY Timestamp ASC")
-        reminders = await cursor.fetchall()
+       	reminders = await self.db.raw_exec_select("SELECT * FROM reminders ORDER BY Timestamp ASC")
         for reminder in reminders:
             user, timestamp, remindMsg, channel, message = reminder
             if (timestamp > int(time())):
-                channelObj = await bot.fetch_channel(channel)
+                channelObj = await self.bot.fetch_channel(channel)
                 url = f"https://discord.com/channels/{channelObj.guild.id}/{channel}/{message}"
                 embed = Embed(title=parse.unquote(remindMsg), description=f"""This is your reminder.
 If you want to know the context, [here]({url}) is the link.""")
                 await channelObj.send(f"<@{user}>", embed=embed)
-                await cursor.execute("DELETE FROM reminders WHERE User = ? AND Timestamp = ?", (user, timestamp))
-        await db.commit()
-        await cursor.close()
+                await self.db.raw_exec_commit("DELETE FROM reminders WHERE User = ? AND Timestamp = ?", (user, timestamp))
