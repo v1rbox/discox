2e93b8fa29dabafd351efcf3556123bb056412b2
Demir Değerli <demirdegerli@protonmail.com>
2023-01-26 15:04:42 +0300

    fix tabs and spaces

--- bot/__main__.py
+++ bot/__main__.py
@@ -246,16 +246,16 @@ def main() -> None:
        	await cursor.execute("SELECT * FROM reminders ORDER BY Timestamp ASC")
         reminders = await cursor.fetchall()
 	for reminder in reminders:
-		user, timestamp, remindMsg, channel, message = reminder
-        	channelObj = await bot.fetch_channel(channel)
-            	url = f"https://discord.com/channels/{channelObj.guild.id}/{channel}/{message}"
-	    	embed = Embed(title=parse.unquote(remindMsg), description=f"""This is your reminder.
+	    user, timestamp, remindMsg, channel, message = reminder
+            channelObj = await bot.fetch_channel(channel)
+            url = f"https://discord.com/channels/{channelObj.guild.id}/{channel}/{message}"
+	    embed = Embed(title=parse.unquote(remindMsg), description=f"""This is your reminder.
 If you want to know the context, [here]({url}) is the link.""")
-	    	if (timestamp > int(time())):
-			embed = Embed(title=parse.unquote(remindMsg), description=f"""This was your reminder from (<t:{timestamp}:R>) Sorry for being late.
+	    if (timestamp > int(time())):
+	        embed = Embed(title=parse.unquote(remindMsg), description=f"""This was your reminder from (<t:{timestamp}:R>) Sorry for being late.
 If you want to know the context, [here]({url}) is the link.""")
-            	await channelObj.send(f"<@{user}>", embed=embed)
-            	await cursor.execute("DELETE FROM reminders WHERE User = ? AND Timestamp = ?", (user, timestamp))
+            await channelObj.send(f"<@{user}>", embed=embed)
+            await cursor.execute("DELETE FROM reminders WHERE User = ? AND Timestamp = ?", (user, timestamp))
         await db.commit()
         await cursor.close()
 
