8a9cba52e4bdb87e524a6b1b8a33e51122ed79a3
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-01-29 09:01:26 +0000

    Added embed and user mention argument

--- bot/commands/utility/avatar.py
+++ bot/commands/utility/avatar.py
@@ -1,3 +1,5 @@
+from re import findall
+
 from bot.base import Command
 from bot.config import Config, Embed
 
@@ -6,9 +8,19 @@ class cmd(Command):
     """A discord command instance."""
 
     name = "avatar"
-    usage = "avatar"
-    description = "Returns the avatar of the user.."
+    usage = "avatar [mention]"
+    description = "Returns the avatar of the user. If a mention or id is given, returns the avatar of that user"
 
     async def execute(self, arguments, message) -> None:
-        url = message.author.avatar.url
-        await message.reply(url)
+        if len(arguments):
+            userId = int("".join(findall("\d", arguments[0])))
+            user = await message.guild.fetch_member(userId)
+        else:
+            user = message.author
+
+        url = user.display_avatar.url
+        embed = Embed(
+            title=f"Avatar of {str(user)}"
+        )
+        embed.set_image(url=url)
+        await message.reply(embed=embed)
