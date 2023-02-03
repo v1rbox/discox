9510934acd7f18845909b54ca52a5196f2318e35
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-02-03 16:45:32 +0100

    Added type checking

--- bot/__main__.py
+++ bot/__main__.py
@@ -129,6 +129,331 @@ async def parse_usage_text(
     return True
 
 
+async def parse_types(
+    usage: str, arguments: List[str], message: discord.message
+) -> List:
+    usage_raw = re.findall(r"<.*?>|\[(.*?)(?::(\w+))?\]", usage)
+    usage_parsed = []
+    for i in usage_raw:
+        if i[1] is None:
+            usage_parsed.append([i[0], "str"])
+        else:
+            usage_parsed.append([i[0], i[1]])
+    result = []
+    for i in range(len(arguments)):
+        match usage_parsed[i][1]:
+            case "member":
+                try:
+                    user = message.guild.get_member_named(arguments[i])
+                    assert user is not None
+                except AssertionError:
+                    try:
+                        user = await message.guild.fetch_member(arguments[i])
+                    except (discord.NotFound, discord.HTTPException, discord.Forbidden):
+                        try:
+                            user = await message.guild.fetch_member(
+                                message.mentions[0].id
+                            )
+                        except IndexError:
+                            embed = Embed(
+                                title="No mentions associated with any of users in this server.",
+                                description=f"The user mentioned `{arguments[0]}` not found.\n*Note: This command is case sensitive. E.g use `Virbox#2050` instead of `virbox#2050`.*",
+                            )
+                            embed.set_color("red")
+                            await message.channel.send(embed=embed)
+                            return False
+                        except (
+                            discord.NotFound,
+                            discord.HTTPException,
+                            discord.Forbidden,
+                        ):
+                            embed = Embed(
+                                title="User not found",
+                                description=f"The user named `{arguments[0]}` not found.\n*Note: This command is case sensitive. E.g use `Virbox#2050` instead of `virbox#2050`.*",
+                            )
+                            embed.set_color("red")
+                            await message.channel.send(embed=embed)
+                            return False
+                result.append(user)
+            case _:
+                result.append(arguments[i])
+    return result
+
+
+def main() -> None:
+    """Main setup function."""
+
+    db = SQLParser("bot/assets/main.db", CREATE_STATEMENTS)
+    bot = discord.Client(intents=discord.Intents.all())
+
+    bot.manager = CommandsManager(bot, db)
+    bot.event_manager = EventsManager(bot, db)
+    tasks_manager = TasksManager(bot, db)
+
+    # Start autoreloads
+    threading.Thread(target=PoolingManager, args=(bot,)).start()
+
+    async def register_all():
+        # Stop the bot attempting to load the commands multiple times
+        if len(bot.manager.commands) != 0:
+            # bot.manager.reset()
+            # bot.event_manager.reset()
+            # tasks_manager.reset()
+            return
+
+        # Load the commands
+        entries = [
+            i
+            for i in os.listdir(os.path.join("bot", "commands"))
+            if not i.startswith("__")
+        ]
+        for entry in entries:
+            cmd = entry.split(".")[0]
+            if os.path.isfile(os.path.join("bot", "commands", entry)):
+                bot.manager.register(
+                    __import__(
+                        f"bot.commands.{cmd}", globals(), locals(), ["cmd"], 0
+                    ).cmd,
+                    file=entry,
+                )
+            else:
+                # Current entry is a category
+                for cmd in [
+                    i.split(".")[0]
+                    for i in os.listdir(os.path.join("bot", "commands", entry))
+                    if not i.startswith("__")
+                ]:
+                    bot.manager.register(
+                        __import__(
+                            f"bot.commands.{entry}.{cmd}",
+                            globals(),
+                            locals(),
+                            ["cmd"],
+                            0,
+                        ).cmd,
+                        entry,
+                        file=os.path.join(entry, cmd + ".py"),
+                    )
+
+        # Setup events
+        entries = [
+            i.split(".")[0]
+            for i in os.listdir(os.path.join("bot", "events"))
+            if not i.startswith("__")
+        ]
+        for idx, entry in zip(range(1, len(entries) + 1, 1), entries):
+            event = __import__(
+                f"bot.events.{entry}", globals(), locals(), ["event"], 0
+            ).event
+            bot.event_manager.register(event)
+
+        # Setup tasks
+        entries = [
+            i.split(".")[0]
+            for i in os.listdir(os.path.join("bot", "tasks"))
+            if not i.startswith("__")
+        ]
+        for idx, entry in zip(range(1, len(entries) + 1, 1), entries):
+            imp = __import__(f"bot.tasks.{entry}", globals(), locals(), ["*"], 0)
+            task = [getattr(imp, i) for i in dir(imp) if i.endswith("Loop")][0]
+
+            tasks_manager.register(task)
+
+    bot.register_all = register_all
+
+    @bot.event
+    async def on_ready():
+        """When the bot is connected."""
+
+        await db.initialise()
+
+        if bot.user is None:
+            raise RuntimeError("Bot user is None")
+
+        logger.log("Bot is ready!")
+        logger.log(f"Logged in as {bot.user}")
+
+        logger.newline()
+        logger.log("Username:", f"{bot.user.name}#{bot.user.discriminator}")
+        logger.log("ID:", f"{bot.user.id}")
+        logger.log("Guilds:", f"{len(bot.guilds)}")
+        logger.log("Prefix:", config.prefix)
+        logger.newline()
+
+        activity = discord.Activity(
+            type=discord.ActivityType.watching, name=f"Virbox videos"
+        )
+
+        await bot.register_all()
+
+        await bot.change_presence(activity=activity)
+        bot.current_activity = activity
+        bot.current_status = discord.Status.online
+
+    @bot.event
+    async def on_message(message: discord.Message):
+        """Handle incoming messages."""
+        if (
+            message.author == bot.user
+            or not message.content.startswith(config.prefix)
+            or not bot.is_ready()
+        ):
+            await bot.event_manager.event_map()["on_message"].execute(message)
+            return
+
+        command, arguments = parse_user_input(message.content[len(config.prefix) :])
+
+        # Check for category
+        prefixes: List[str] = [i.prefix for i in bot.manager.categories]
+        if command in prefixes:
+            try:
+                cmdobj = {
+                    i.prefix: i for i in bot.manager.categories if i.prefix is not None
+                }[command].commands_map()[arguments[0]]
+            except KeyError:
+                await logger.send_error(
+                    f"Command '{command} {arguments[0]}' not found", message
+                )
+                return
+            except IndexError:
+                await logger.send_error(
+                    f"No subcommand found, use '{config.prefix}help {command}' for more information",
+                    message,
+                )
+                return
+
+            command = arguments[0]
+            arguments = arguments[1:]
+        else:
+            try:
+                cmdobj = bot.manager[command]
+            except KeyError:
+                try:
+                    cmdobj = [
+                        [c for c in i.commands if c.name == command]
+                        for i in bot.manager.categories
+                        if i.prefix is None
+                    ]
+                    cmdobj = [i for i in cmdobj if len(i) != 0][0][0]
+                except IndexError:
+                    await logger.send_error(f"Command '{command}' not found", message)
+                    return
+
+        logger.log(
+            f"'{message.author}' issued command '{command}'",
+            f"with arguments: {arguments}",
+        )
+
+        if cmdobj.category is not None:
+            if not cmdobj.category.check_permissions(message):
+                logger.error("Insufficient permissions.")
+                await logger.send_error("Insufficient permissions.", message)
+                return
+            if (
+                len(cmdobj.category.channels)
+                and not message.channel.id in cmdobj.category.channels
+            ):
+                logger.error("Channel not allowed.")
+                await logger.send_error("Channel not allowed.", message)
+                return
+
+        # Join args
+        usage_args: List[Tuple[str, str]] = re.findall(
+            f"\[([^\[\]]+)\]|\<([^\<\>]+)\>", cmdobj.usage
+        )
+        args_raw: List[str] = [f"<{i[1]}>" if i[1] else f"[{i[0]}]" for i in usage_args]
+
+        if len(args_raw) >= 1 and args_raw[-1][1] == "*":
+            args, tmp = (arguments[: len(args_raw) - 1], arguments[len(args_raw) - 1 :])
+            arguments = args + [" ".join(tmp)]
+
+        # Check if a valid number of arguments have been passed
+        if await parse_usage_text(cmdobj.usage, arguments, message):
+            arguments_typed = await parse_types(cmdobj.usage, arguments, message)
+            if not arguments_typed:
+                return
+            try:
+                await cmdobj.execute(arguments_typed, message)
+            except Exception as e:
+                await logger.send_error(str(e), message)
+                print(traceback.format_exc())
+
+    bot.run(config.token)
+
+
+if __name__ == "__main__":
+    main()
+    """Parse user input
+
+    [Args]:
+        user_input (str): user input
+
+    [Returns]:
+        command_name (str): the command name
+        args (List[str]): command args
+    """
+
+    command_name, *args = user_input.split()
+    args = [
+        tuple(group for group in tpl if group)[0]
+        for tpl in re.findall(
+            r'"([^"]+)"|\'([^\']+)\'|\`\`\`([^\']+)\`\`\`|(\S+)', " ".join(args)
+        )
+    ]
+
+    return command_name, args
+
+
+# TODO for 'args', do a struct 'Argument' with a 'required' bool field
+async def parse_usage_text(
+    usage: str, args: List[str], message: discord.Message
+) -> bool:
+    """Get the usage of a command into a more workable format
+
+    [Args]:
+        usage (str): command usage (help)
+        args (List[str]): command arguments
+        message (discord.Message): discord message
+
+    [Returns]:
+        (bool): True if all went good, False otherwise
+    """
+
+    required: List[str] = re.findall(f'<([^"]+)>', usage)
+    optional: List[str] = re.findall(f'\[([^"]+)\]', usage)
+
+    usage_args: List[Tuple[str, str]] = re.findall(
+        f"\[([^\[\]]+)\]|\<([^\<\>]+)\>", usage
+    )
+    args_raw: List[str] = [f"<{i[1]}>" if i[1] else f"[{i[0]}]" for i in usage_args]
+
+    # Check for missing required arguments
+    if len(args) < len(required):
+        missing = required[len(args)]
+        indx = usage.index(missing)
+        errmsg = f"{config.prefix}{usage}\n{' '*(indx+len(config.prefix)-1)}{'^'*(len(missing)+2)}"
+
+        embed = Embed(
+            title="Error in command syntax",
+            description=f"Missing required argument '{missing}'\n```{errmsg}```",
+        )
+        embed.set_color("red")
+        await message.channel.send(embed=embed)
+        return False
+
+    # Check if the given amount of arguments exceeds the expected amount
+    if len(args) > len(required) + len(optional) and args_raw[-1][1] != "*":
+        embed = Embed(
+            title="Error in command syntax",
+            description=f"Expected `{len(required) + len(optional)}` argument{'s' if len(required) + len(optional) > 1 else ''} but got `{len(args)}`.\nCommand usage: ```{config.prefix}{usage}```",
+        )
+        embed.set_color("red")
+        await message.channel.send(embed=embed)
+        return False
+
+    return True
+
+
 def main() -> None:
     """Main setup function."""
 
