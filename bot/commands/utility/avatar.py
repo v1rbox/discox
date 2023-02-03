d5fc648679969dbad538a4bb908bf4b106baa240
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-02-03 22:06:31 +0000

    Improved and removed random code

--- bot/commands/utility/avatar.py
+++ bot/commands/utility/avatar.py
@@ -12,17 +12,6 @@ class cmd(Command):
     async def execute(self, arguments, message) -> None:
         user = message.author if not len(arguments) else arguments[0]
         url = user.display_avatar.url
-        embed = Embed(title=f"Avatar of {str(user)}")
+        embed = Embed(title=f"{user.name}'s avatar")
         embed.set_image(url=url)
         await message.reply(embed=embed)
-                    except (discord.NotFound, discord.HTTPException, discord.Forbidden):
-                        embed = Embed(
-                            title="User not found",
-                            description=f"The user named `{arguments[0]}` not found.\n*Note: This command is case sensitive. E.g use `Virbox#2050` instead of `virbox#2050`.*",
-                        )
-                        embed.set_color("red")
-                        await message.channel.send(embed=embed)
-                        return
-        embed = Embed(title=f"{user.name}'s avatar")
-        embed.set_image(url=user.avatar.url if user.avatar else user.default_avatar.url)
-        await message.channel.send(embed=embed)
