497fa8eb204120db216c24c7429a7b0c3f1fd3f4
justinlime <justinlime1999@gmail.com>
2023-02-04 00:07:53 -0600

    Edit

--- bot/commands/distroroles/whitelist.py
+++ bot/commands/distroroles/whitelist.py
@@ -1,5 +1,5 @@
+from bot.commands.distroroles.add import DistroRoles
 from bot.base import Command
-from bot.commands.distroroles.add import cmd as Distro
 from bot.config import Config, Embed
 
 
@@ -11,15 +11,5 @@ class cmd(Command):
     description = f"Shows available distro role options"
 
     async def execute(self, arguments, message) -> None:
-        if len(Distro.whitelist) > 0:
-            description = "**Available distro roles:**\n\n"
-            for i in Distro.whitelist:
-                description += f"`{i}`\n"
-            embed = Embed(title="Distro", description=description)
-            await message.channel.send(embed=embed)
-        else:
-            embed = Embed(
-                title="Distro", description="**No distros currently whitelisted**"
-            )
-            embed.set_color("red")
-            await message.channel.send(embed=embed)
+        distroroles = DistroRoles()
+        distroroles.getWhitelist()
