3d6c5700204ef40a53005f86e1b2435f5fec2257
Demir Değerli <demirdegerli@protonmail.com>
2023-02-05 14:53:46 +0300

    add db support to starboard and improve the embed

--- bot/events/reaction_add.py
+++ bot/events/reaction_add.py
@@ -8,7 +8,7 @@ class event(Event):
     name = "on_raw_reaction_add"
 
     async def execute(self, payload) -> None:
-        REACTION = "star"
+        REACTION = "⭐"
         if not payload.emoji.name == REACTION:
             return
         channelObj = await self.bot.fetch_channel(payload.channel_id)
@@ -16,9 +16,32 @@ class event(Event):
         for reaction in messageObj.reactions:
             if not reaction == REACTION:
                 pass
-            if reaction.count == 5:
-                starboard = await self.bot.fetch_channel(Config.starboard_channel)
-                embed = Embed(
-                    
+            if reaction.count >= 5:
+                already = await self.db.raw_exec_select(
+                    "SELECT message_id FROM starboard WHERE message_id = ?",
+                    (messageObj.id,)
                 )
-                await starboard.send(embed=embed)
+                if len(already) == 0:
+                    starboard = await self.bot.fetch_channel(Config.starboard_channel)
+                    embed = Embed()
+                    embed.set_color("yellow")
+                    embed.set_footer(
+                        text=f'{messageObj.author} • {reaction.count} stars',
+                        icon_url=messageObj.author.display_avatar.url
+                    )
+                    if messageObj.content:
+                        embed.title = messageObj.content
+                    if len(messageObj.attachments) != 0:
+                        if "image" in messageObj.attachments[0].content_type:
+                            embed.set_image(url=messageObj.attachments[0].url)
+                            await starboard.send(embed=embed)
+                        else:
+                            content = ''
+                            if messageObj.content:
+                                content += f'{messageObj.content} • '
+                            content += f'{reaction.count} stars • by {messageObj.author}\n{messageObj.attachments[0].url}'
+                            await starboard.send(content)
+                    await self.db.raw_exec_commit(
+                        """INSERT INTO starboard(message_id) VALUES (?)""",
+                        (messageObj.id,)
+                    )
