2156f0c56842160cd3ae0b229b67bceae187959e
logicguy1 <logicguy.mailandcontact@gmail.com>
2023-02-05 16:27:27 +0100

    Added dynamic voice channels

---
+++ bot/events/on_voice_state.py
@@ -0,0 +1,21 @@
+from bot.base import Event
+from bot.config import Config, Embed
+
+
+class event(Event):
+    """A discord event instance."""
+
+    name = "on_voice_state_update"
+
+    async def execute(self, member, before, after) -> None:
+        if after.channel is not None and "Create VC" in after.channel.name:
+            category = after.channel.category
+            if len(category.voice_channels) > 6:
+                await member.move_to(None, reason="No more avaliable voice channels")
+                await member.send("No more avaliable voice channels.")
+            else:
+                channel = await category.create_voice_channel(f"Voice Channel #{len(category.voice_channels)}")
+                await member.move_to(channel, reason="Created private channel")
+
+        elif "Voice Channel #" in before.channel.name and len(before.channel.members) == 0:
+            await before.channel.delete(reason="Empty channel")
