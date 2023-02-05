71fb735c1697a87c41fa2bf02f4362b4b2c83597
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-02-05 10:59:26 +0000

    Added starboard

--- bot/events/reaction_add.py
+++ bot/events/reaction_add.py
@@ -8,4 +8,17 @@ class event(Event):
     name = "on_raw_reaction_add"
 
     async def execute(self, payload) -> None:
-        pass
+        REACTION = "star"
+        if not payload.emoji.name == REACTION:
+            return
+        channelObj = await self.bot.fetch_channel(payload.channel_id)
+        messageObj = await channelObj.fetch_message(payload.message_id)
+        for reaction in messageObj.reactions:
+            if not reaction == REACTION:
+                pass
+            if reaction.count == 5:
+                starboard = await self.bot.fetch_channel(Config.starboard_channel)
+                embed = Embed(
+                    
+                )
+                await starboard.send(embed=embed)
