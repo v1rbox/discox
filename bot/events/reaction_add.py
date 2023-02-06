5eb16f0186a62b62ef8cff54cbcc7a1e0d261a11
Code beautifier <logicguy1@users.noreply.github.com>
2023-02-06 11:06:17 +0000

    Beautified

--- bot/events/reaction_add.py
+++ bot/events/reaction_add.py
@@ -1,4 +1,4 @@
-from re import sub, search
+from re import search, sub
 
 from bot.base import Event
 from bot.config import Config, Embed
@@ -23,29 +23,42 @@ class event(Event):
             if reaction.count >= 5:
                 already = await self.db.raw_exec_select(
                     "SELECT message_id FROM starboard WHERE message_id = ?",
-                    (messageObj.id,)
+                    (messageObj.id,),
                 )
                 if len(already) == 0:
                     board_message = None
                     embed = Embed()
                     embed.set_color("yellow")
                     embed.set_author(
-                        name=f'{messageObj.author}',
-                        url=f'https://discord.com/users/{messageObj.author.id}',
-                        icon_url=messageObj.author.display_avatar.url
+                        name=f"{messageObj.author}",
+                        url=f"https://discord.com/users/{messageObj.author.id}",
+                        icon_url=messageObj.author.display_avatar.url,
                     )
-                    embed.add_field(name="Original", value=f'[Jump!]({messageObj.jump_url})')
+                    embed.add_field(
+                        name="Original", value=f"[Jump!]({messageObj.jump_url})"
+                    )
 
                     if messageObj.content:
                         embed.description = messageObj.content
                     if len(messageObj.attachments) != 0:
                         if "image" in messageObj.attachments[0].content_type:
                             embed.set_image(url=messageObj.attachments[0].url)
-                            board_message = await starboard.send(content=f'{REACTION} **{reaction.count}**', embed=embed)
+                            board_message = await starboard.send(
+                                content=f"{REACTION} **{reaction.count}**", embed=embed
+                            )
                         else:
-                            attachment_name = sub("http(s)?:\/\/.*\/attachments\/.*\/", "", messageObj.attachments[0].url)
-                            embed.add_field(name="Attachment", value=f'[{attachment_name}]({messageObj.attachments[0].url})')
-                            board_message = await starboard.send(content=f'{REACTION} **{reaction.count}**', embed=embed)
+                            attachment_name = sub(
+                                "http(s)?:\/\/.*\/attachments\/.*\/",
+                                "",
+                                messageObj.attachments[0].url,
+                            )
+                            embed.add_field(
+                                name="Attachment",
+                                value=f"[{attachment_name}]({messageObj.attachments[0].url})",
+                            )
+                            board_message = await starboard.send(
+                                content=f"{REACTION} **{reaction.count}**", embed=embed
+                            )
 
                     else:
                         direct_image_link = search(IMAGE_REGEX, messageObj.content)
@@ -54,26 +67,34 @@ class event(Event):
                             if len(sub(IMAGE_REGEX, "", messageObj.content)) == 0:
                                 embed.description = None
                             else:
-                                embed.description = sub(IMAGE_REGEX, "[image link]", messageObj.content)
-                        board_message = await starboard.send(content=f'{REACTION} **{reaction.count}**', embed=embed)
+                                embed.description = sub(
+                                    IMAGE_REGEX, "[image link]", messageObj.content
+                                )
+                        board_message = await starboard.send(
+                            content=f"{REACTION} **{reaction.count}**", embed=embed
+                        )
                     await self.db.raw_exec_commit(
                         """INSERT INTO starboard(message_id, board_message_id) VALUES (?,?)""",
-                        (messageObj.id,board_message.id,)
+                        (
+                            messageObj.id,
+                            board_message.id,
+                        ),
                     )
                 else:
                     board_message_id = await self.db.raw_exec_select(
                         "SELECT board_message_id FROM starboard WHERE message_id = ?",
-                        (messageObj.id,)
+                        (messageObj.id,),
                     )
                     board_message_id = board_message_id[0][0]
                     board_message = await starboard.fetch_message(board_message_id)
                     new_embed = board_message.embeds[0]
                     new_embed.set_author(
-                        name=f'{messageObj.author}',
-                        url=f'https://discord.com/users/{messageObj.author.id}',
-                        icon_url=messageObj.author.display_avatar.url
+                        name=f"{messageObj.author}",
+                        url=f"https://discord.com/users/{messageObj.author.id}",
+                        icon_url=messageObj.author.display_avatar.url,
                     )
                     if messageObj.content:
                         new_embed.description = messageObj.content
-                        await board_message.edit(content=f'{REACTION} **{reaction.count}**', embed=new_embed)
-
+                        await board_message.edit(
+                            content=f"{REACTION} **{reaction.count}**", embed=new_embed
+                        )
