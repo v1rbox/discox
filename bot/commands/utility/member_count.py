b5f569e1619dcf8232551319c1d1059c5341178e
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-02-16 15:30:30 +0100

    Added message when count reaches 1k

--- bot/commands/utility/member_count.py
+++ bot/commands/utility/member_count.py
@@ -10,11 +10,12 @@ class cmd(Command):
     description = "Get the current membercount in the server."
 
     async def execute(self, arguments, message) -> None:
+        memberCount = message.guild.member_count
         result = await self.db.raw_exec_select(f"SELECT membercount FROM membercount")
         if len(result) == 0:
             await self.db.raw_exec_commit(
                 f"INSERT INTO membercount(membercount) VALUES(?)",
-                (message.guild.member_count,),
+                (memberCount,),
             )
         result = await self.db.raw_exec_select(f"SELECT membercount FROM membercount")
         status = "increased"
@@ -24,12 +25,18 @@ class cmd(Command):
             status = "decreased"
             isgood = ""
         if result[0][0] != message.guild.member_count:
-            extra = f"\nThe member count has {status} by {abs(message.guild.member_count - result[0][0])} since last execution {isgood}"
+            extra = f"\nThe member count has {status} by {abs(memberCount - result[0][0])} since last execution {isgood}"
         embed = Embed(
             title="Member Count",
-            description=f"Currently the server has `{message.guild.member_count}` members.{extra}",
+            description=f"Currently the server has `{memberCount}` members.{extra}",
         )
         await message.channel.send(embed=embed)
         await self.db.raw_exec_commit(
-            f"UPDATE membercount SET membercount = ?", (message.guild.member_count,)
+            f"UPDATE membercount SET membercount = ?", (memberCount,)
         )
+        if memberCount >= 1000 and memberCount < 1050:
+            embed = Embed(
+                title=":tada: We did it! :tada:",
+                description="We have hit 1000 members. Thank you to everyone who joined!"
+            )
+            await message.channel.send(embed=embed)
