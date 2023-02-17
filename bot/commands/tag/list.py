7f635f68e91b9b89e6f14b427dd3fc72346a74f5
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-02-17 22:43:19 +0100

    added tag commands

---
+++ bot/commands/tag/list.py
@@ -0,0 +1,24 @@
+from urllib.parse import unquote
+from bot.base import Command
+from bot.config import Config, Embed
+
+
+class cmd(Command):
+    """A discord command instance."""
+
+    name = "list"
+    usage = "list"
+    description = """Lists all tags"""
+
+    async def execute(self, arguments, message) -> None:
+        tags = await self.db.raw_exec_select("SELECT * FROM tags")
+        embed = Embed(
+            title="Tags"
+        )
+        for tag in tags:
+            name, content = tag
+            embed.add_field(
+                name=f"{unquote(name)}",
+                value=f"{unquote(content)}",
+            )
+        await message.reply(embed=embed)
