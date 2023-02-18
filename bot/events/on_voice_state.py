ae567ca0d63d3fcf2778048079b718d720f825e8
Demir Değerli <demirdegerli@protonmail.com>
2023-02-19 02:02:47 +0300

    [fix] always check `before.channel`

--- bot/events/on_voice_state.py
+++ bot/events/on_voice_state.py
@@ -8,6 +8,8 @@ class event(Event):
     name = "on_voice_state_update"
 
     async def execute(self, member, before, after) -> None:
+        if "Voice Channel #" in before.channel.name and len(before.channel.members) == 0:
+            await before.channel.delete(reason="Empty channel")
         if after.channel is not None and "Create VC" in after.channel.name:
             category = after.channel.category
             if len(category.voice_channels) > 6:
@@ -18,9 +20,3 @@ class event(Event):
                     f"Voice Channel #{len(category.voice_channels)}"
                 )
                 await member.move_to(channel, reason="Created private channel")
-
-        elif (
-            "Voice Channel #" in before.channel.name
-            and len(before.channel.members) == 0
-        ):
-            await before.channel.delete(reason="Empty channel")
