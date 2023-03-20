db494b761813c7d0211acca8ccc1e48ec60a496f
Code beautifier <imindMan@users.noreply.github.com>
2023-03-20 13:59:47 +0000

    Beautified

--- bot/commands/fun/gigachad.py
+++ bot/commands/fun/gigachad.py
@@ -3,6 +3,7 @@ import random
 from bot.base import Command
 from bot.config import Embed
 
+
 class cmd(Command):
     """A discord command instance."""
 
@@ -11,29 +12,29 @@ class cmd(Command):
     description = "Gigachad random image"
 
     async def execute(self, arguments, message) -> None:
-        # Special thanks to the awesome work of justinlime for the collection of gigachads 
+        # Special thanks to the awesome work of justinlime for the collection of gigachads
         # Repo: https://github.com/justinlime/GigaChads
 
-        # Start parsing every available images in the GigaChads repo 
+        # Start parsing every available images in the GigaChads repo
         list_of_images = [
-
-                    "gigabigbrain.jpg",
-                    "gigacrab.png",
-                    "gigadesktop.jpg",
-                    "gigalaptop.jpg",
-                    "gigalegs.jpg",
-                    "gigamirror.jpg",
-                    "gigamutant.jpg",
-                    "gigaphone.jpg",
-                    "gigatriple.jpg",
-                    "gigatriplesupreme.jpg",
-                    "gigawide.jpg"
-
-                ]
+            "gigabigbrain.jpg",
+            "gigacrab.png",
+            "gigadesktop.jpg",
+            "gigalaptop.jpg",
+            "gigalegs.jpg",
+            "gigamirror.jpg",
+            "gigamutant.jpg",
+            "gigaphone.jpg",
+            "gigatriple.jpg",
+            "gigatriplesupreme.jpg",
+            "gigawide.jpg",
+        ]
         # randomly
         image = random.choice(list_of_images)
 
         # send the image
-        embed = Embed(title="Best GigaChad ever:") 
-        embed.set_image(url=f"https://raw.githubusercontent.com/justinlime/GigaChads/main/{image}")
+        embed = Embed(title="Best GigaChad ever:")
+        embed.set_image(
+            url=f"https://raw.githubusercontent.com/justinlime/GigaChads/main/{image}"
+        )
         await message.channel.send(embed=embed)
