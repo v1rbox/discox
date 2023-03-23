c9431bfe0557412efa032f29fb06a139debc82e7
justinlime <justinlime1999@gmail.com>
2023-03-23 02:15:35 -0500

    add support for gigalist.json

--- bot/commands/fun/gigachad.py
+++ bot/commands/fun/gigachad.py
@@ -1,3 +1,5 @@
+import json
+import aiohttp
 import random
 
 from bot.base import Command
@@ -14,27 +16,24 @@ class cmd(Command):
     async def execute(self, arguments, message) -> None:
         # Special thanks to the awesome work of justinlime for the collection of gigachads
         # Repo: https://github.com/justinlime/GigaChads
-
+        
         # Start parsing every available images in the GigaChads repo
-        list_of_images = [
-            "gigabigbrain.jpg",
-            "gigacrab.png",
-            "gigadesktop.jpg",
-            "gigalaptop.jpg",
-            "gigalegs.jpg",
-            "gigamirror.jpg",
-            "gigamutant.jpg",
-            "gigaphone.jpg",
-            "gigatriple.jpg",
-            "gigatriplesupreme.jpg",
-            "gigawide.jpg",
-        ]
+        gigaurl = "https://raw.githubusercontent.com/justinlime/GigaChads/main/gigalist.json"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(gigaurl) as result:
+                if result.status != 200:
+                    embed = Embed(title="GigaSad :(",description="Could Not Load")
+                    embed.set_color("red")
+                    await message.channel.send(embed=embed)
+                    return
+                gigalist = await result.json()["gigachads"]
+
         # randomly
-        image = random.choice(list_of_images)
+        gigachad = random.choice(gigalist)
 
         # send the image
         embed = Embed(title="Best GigaChad ever:")
         embed.set_image(
-            url=f"https://raw.githubusercontent.com/justinlime/GigaChads/main/{image}"
+            url=f"https://raw.githubusercontent.com/justinlime/GigaChads/main/gigachads/{gigachad}"
         )
         await message.channel.send(embed=embed)
