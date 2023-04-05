40e46b225f6c978b968d33022c5413010143ce62
Neptuneia <57576932+Neptuneia@users.noreply.github.com>
2023-04-05 11:33:46 -0600

    blocked prompts for gen.py uwu

---
+++ bot/events/blocked_prompts.py
@@ -0,0 +1,26 @@
+blocked = [
+    "nsfw",
+    "pussy",
+    "gore",
+    "murder",
+    "penis",
+    "cat",
+    "emacs",
+    "porn",
+    "vagina",
+    "dick",
+    "sex",
+    "intercourse",
+    "bondage",
+    "bdsm",
+    "fetish",
+    "kink",
+    "pegging",
+    "peg",
+    "anal",
+    "vaginal",
+    "erotic"
+
+    
+
+]
