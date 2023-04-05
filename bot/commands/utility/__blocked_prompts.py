fb7a8c0ce2acf70b59c235c1179721433bc3df8e
Neptuneia <57576932+Neptuneia@users.noreply.github.com>
2023-04-05 15:37:49 -0600

    moved it here uwu

---
+++ bot/commands/utility/__blocked_prompts.py
@@ -0,0 +1,25 @@
+blocked = [
+    "nsfw",
+    "pussy",
+    "gore",
+    "murder",
+    "penis",
+    "cat",
+    "emacs",
+    "porn",
+    "vagina",
+    "dick",
+    "sex",
+    "intercourse",
+    "bondage",
+    "bdsm",
+    "fetish",
+    "kink",
+    "pegging",
+    "peg",
+    "anal",
+    "vaginal",
+    "erotic",
+    "notepad"
+
+]
