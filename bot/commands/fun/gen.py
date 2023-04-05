4894080818dcfaf460e4be926b64cf6ec73d4d29
Neptuneia <57576932+Neptuneia@users.noreply.github.com>
2023-04-05 13:55:38 -0600

    updated with EVEN BETTER code

--- bot/commands/fun/gen.py
+++ bot/commands/fun/gen.py
@@ -1,28 +1,33 @@
-from craiyon import Craiyon
-from bot.config import Config, Embed
-from bot.base import Command
 from io import BytesIO
 import base64
 import time
-from discox.bot.events.blocked_prompts import blocked
+from craiyon import Craiyon
+from bot.commands.fun.__blocked_prompts import blocked
+from bot.config import Config, Embed
+from bot.base import Command
+
 
 class cmd(Command):
     """ A discord command instance. """
-
+# 
     name = "gen"
     usage = "gen <*prompt>"
     description = "Uses the Craiyon AI API to generate an image with a given prompt"
 
-    async def execute(self, arguments, message, prompt) -> None:
+    async def execute(self, arguments, message) -> None:
+        prompt = arguments[0]
         if prompt.lower() in blocked:
             await message.channel.send("I'm not generating that you meanie >:( It's NSFW! Try something else!")
             return
+        
+        loadingE = Embed(title=f"Image {prompt} currently generating, please wait a moment!")
+        loadingM = await message.channel.send(embed=loadingE)
             
         generator = Craiyon()
         result = generator.generate(prompt)
         images = result.images
         embed = Embed(title="Here is the generated image")
-        embed.set_image(images)
+        embed.set_image(url=images[0])
         
-
+        await loadingM.delete()
         await message.channel.send(embed=embed)
