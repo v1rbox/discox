bdc3ab1175d505feda955692ee2a874eacc6c089
justinlime <justinlime1999@gmail.com>
2023-07-22 18:28:51 -0500

    Prevent api spam

--- bot/tasks/activity.py
+++ bot/tasks/activity.py
@@ -6,16 +6,23 @@ from mcstatus import JavaServer
 
 
 class TaskLoop(Task):
+    current = 0
     @loop(minutes=10)
     async def execute(self) -> None:
         server = JavaServer.lookup(f"{Config.minecraft_url}:{Config.minecraft_port}")
         status = server.status()
-        if status.players.online > 6:
-            activity = discord.Activity(
-                type=discord.ActivityType.playing, name=f"Minecraft {status.players.online} Online"
-            )
+
+        # Prevent discord api spam
+        if status.players.online == self.current:
+            return
         else:
-            activity = discord.Activity(
-                type=discord.ActivityType.watching, name=f"Virbox videos"
-            )
-        await self.bot.change_presence(activity=activity)
+            if status.players.online > 6:
+                activity = discord.Activity(
+                    type=discord.ActivityType.playing, name=f"with {status.players.online} Virbcrafters"
+                )
+            else:
+                activity = discord.Activity(
+                    type=discord.ActivityType.watching, name=f"Virbox videos"
+                )
+            self.current = status.players.online
+            await self.bot.change_presence(activity=activity)
