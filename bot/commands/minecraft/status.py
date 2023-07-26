8ed94c50f51991452d258c33098955a51441d18d
justinlime <justinlime1999@gmail.com>
2023-07-26 08:54:54 -0500

    Improvements

--- bot/commands/minecraft/status.py
+++ bot/commands/minecraft/status.py
@@ -5,24 +5,24 @@ from bot.config import Config, Embed
 
 
 class cmd(Command):
-    name = "minecraft"
-    usage = "minecraft"
+    name = "status"
+    usage = "status"
     description = "Get the status of the minecraft server"
 
     async def execute(self, arguments, message) -> None:
         server = JavaServer.lookup(f"{Config.minecraft_url}:{Config.minecraft_port}")
-        status = server.status()
-        if Config.minecraft_port == "25565":
-            url = Config.minecraft_url
-        else:
-            url = f"{Config.minecraft_url}:{Config.minecraft_port}"
+# 'query' has to be enabled in a server's server.properties file!
+# It may give more information than a ping, such as a full player list or mod information.
+        query = server.query()
+
+        plugins = ' \n'.join(query.software.plugins)
         embed = Embed(
             title="Minecraft",
             description=f"""
-**URL:** ```{url}```
-**VERSION:** ```{status.version.name}```
-**MOTD:** ```{status.motd.to_plain()}```
-**CURRENT PLAYERS:** ```{status.players.online}```
+**URL:** ```{Config.minecraft_url}:{Config.minecraft_port}```
+**VERSION:** ```{query.software.version}({query.software.brand})```
+**PLAYERS:** ```{query.players.online} online```
+**PLUGINS:** ```{plugins}```
 """,
         )
         embed.set_color("green")
