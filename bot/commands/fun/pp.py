22dcb427d946e071161ba50a89af56dc86f2a993
Code beautifier <logicguy1@users.noreply.github.com>
2024-06-05 06:55:59 +0000

    Beautified

--- bot/commands/fun/pp.py
+++ bot/commands/fun/pp.py
@@ -1,6 +1,7 @@
+import random
+
 from bot.base import Command
 from bot.config import Embed
-import random
 
 
 class cmd(Command):
@@ -13,7 +14,7 @@ class cmd(Command):
     async def execute(self, arguments, message) -> None:
         pp = message.author.name if not len(arguments) else arguments[0]
         random.seed(pp)
-        pp_int=random.randint(0, 51)
+        pp_int = random.randint(0, 51)
         pp_size = "8" + "".join("=" for p in range(pp_int)) + "D"
         pp_rating = "smol" if pp_int < 18 else "avg" if pp_int < 36 else "chungo"
         embed = Embed(title=f"{pp}'s pp size \n\n**{pp_size}**\n\n*{pp_rating} pp*\n")
